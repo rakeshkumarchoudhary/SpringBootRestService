80fdb72657c94c2ec54fb851a18d7eb849a7c334
Rakesh <rakeshkumarchoudhary@gmail.com>
2019-03-03 13:00:56 +0530

    packaging changed to war

--- src/main/java/com/java/sboot/springboottest/SpringbootTestApplication.java
+++ src/main/java/com/java/sboot/springboottest/SpringbootTestApplication.java
@@ -2,12 +2,20 @@ package com.java.sboot.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.java.sboot.controllers,com.java.sboot.entity")
-public class SpringbootTestApplication {
+public class SpringbootTestApplication extends SpringBootServletInitializer{
 
+	
+	@Override 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
+ 
+        return application.sources(SpringbootTestApplication.class); 
+	}
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringbootTestApplication.class, args);
 	}}
